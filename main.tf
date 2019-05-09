60ba6e9380f6e555077d64083577777d38063162
mateescu <mateescu@ca.ibm.com>
2019-05-09 18:53:39 -0400

    proevider

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ variable "allow_selfsigned_cert" {
 
 ############### Optinal settings in provider ##########
 provider "vsphere" {
-    version = "~> 1.9"
+    #version = "~> 1.9"
     allow_unverified_ssl = "${var.allow_selfsigned_cert}"
 }
  

5bde2a8ddedbbbcdc413b525fa8f9feda34b24a5
mateescu <mateescu@ca.ibm.com>
2019-05-17 11:34:58 -0400

    10

--- TS002232926/file29.tf
+++ TS002232926/file29.tf
@@ -1,11 +1,4 @@
-##################### Variables ###############################
-
-############### Optinal settings in provider ##########
-provider "vsphere" {
-    #version = "~> 1.9"
-    allow_unverified_ssl = "${var.allow_selfsigned_cert}"
-}
  
-data "vsphere_datacenter" "datacenter" {
+data "vsphere_datacenter" "datacenter29" {
   name = "${var.datacenter}"
 }

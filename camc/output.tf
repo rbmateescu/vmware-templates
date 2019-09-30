f4152a5ba5a33011ec89050fd42602d20bb2f3ac
mateescu <mateescu@ca.ibm.com>
2019-09-30 16:03:22 -0400

    #

--- camc/output.tf
+++ camc/output.tf
@@ -2,6 +2,6 @@ output "create_output" {
 	value = "${camc_scriptpackage.create_test.result}"
 }
 
-output "delete_output" {
-	value = "${camc_scriptpackage.delete_test}"
-}
+#output "delete_output" {
+#	value = "${camc_scriptpackage.delete_test}"
+#}
